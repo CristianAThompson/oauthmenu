ff71341b243518ea3c92948df51fde8d750743db
Cristian Thompson <Thompsonacristian@gmail.com>
2017-01-01 12:56:38 -0400

    adjusted

--- project.py
+++ project.py
@@ -458,4 +458,5 @@ def createUser(login_session):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 33507))
+    app.run(host='0.0.0.0', port=port)
